76e58e91f8c60141535972866eebd464fdcaa594
Soroka bird <vladavlada09756@gmail.com>
2024-09-07 19:46:14 +0300

    added back_ground+collision logic

--- Vampire_survivor/Player.py
+++ Vampire_survivor/Player.py
@@ -1,13 +1,15 @@
 from settings import *
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, groups):
+    def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
         path = join('images', 'player', 'down', '0.png')
         self.image = pygame.image.load(path).convert_alpha()
-        self.rect = self.image.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-        self.direction = pygame.Vector2()
+        self.rect = self.image.get_frect(center = pos)
+        self.hitbox_rect = self.rect.inflate(-40, 0)
+        self.direction = pygame.Vector2(1, 0)
         self.speed = 300
+        self.collision_sprites = collision_sprites
         self.lives = 5
         self.mask = pygame.mask.from_surface(self.image)
 
@@ -17,14 +19,34 @@ class Player(pygame.sprite.Sprite):
     def gain_score(self):
         self.score += 1
 
-    def update(self, dt):
+    def input(self):
         keys = pygame.key.get_pressed()
         self.direction.x = int(keys[pygame.K_d]) + int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_a]) - int(
             keys[pygame.K_LEFT])
         self.direction.y = int(keys[pygame.K_s]) + int(keys[pygame.K_DOWN]) - int(keys[pygame.K_w]) - int(
             keys[pygame.K_UP])
         self.direction = self.direction.normalize() if self.direction else self.direction
-        self.rect.center += self.speed * self.direction * dt  # to reduce power difference in computers
 
+    def move(self, dt):
+        self.hitbox_rect.x += self.direction.x * self.speed * dt
+        self.collision('horizontal')
+        self.hitbox_rect.y += self.direction.y * self.speed * dt
+        self.collision('vertical')
+        self.rect.center = self.hitbox_rect.center
         self.rect.clamp_ip(pygame.Rect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT))
+
+    def collision(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.hitbox_rect):
+                if direction == 'horizontal':
+                    if self.direction.x > 0: self.hitbox_rect.right = sprite.rect.left
+                    if self.direction.x < 0: self.hitbox_rect.left = sprite.rect.right
+                else:
+                    if self.direction.y < 0: self.hitbox_rect.top = sprite.rect.bottom
+                    if self.direction.y > 0: self.hitbox_rect.bottom = sprite.rect.top
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
+
 
--- Vampire_survivor/main.py
+++ Vampire_survivor/main.py
@@ -1,5 +1,9 @@
 from settings import *
 from Player import *
+from Sprites import *
+from pytmx.util_pygame import load_pygame
+
+from random import randint
 
 class Game:
     def __init__(self):
@@ -8,15 +12,19 @@ class Game:
         pygame.display.set_caption('Survivor')
         self.clock = pygame.time.Clock()
         self.running = True
-
         # Sound setup
         self.game_music = pygame.mixer.Sound(join('audio', 'music.wav'))
-        self.game_music.play(loops = -1)
+        # self.game_music.play(loops = -1)
+
         # sprites groups
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
+
+        self.setup()
 
         # creating sprites objects
-        self.player = Player(self.all_sprites)
+        self.player = Player((400,300), self.all_sprites, self.collision_sprites) #collision_sprites additional arg, player do not belong them
+
 
     def run(self):
         while self.running:
@@ -28,7 +36,8 @@ class Game:
                     self.running = False
 
             self.all_sprites.update(dt)
-            self.display_surface.fill('#3a2e3f')  # rosybrown1
+            self.display_surface.fill('#3a2e3f')
+
             self.all_sprites.draw(self.display_surface)
             pygame.display.update()
         pygame.quit()
@@ -36,7 +45,16 @@ class Game:
     def change_sound(self, volume):
         self.game_music.set_volume(volume)
 
+    def setup(self):
+        map = load_pygame(join('data', 'maps', 'world.tmx'))
+        for x, y, image in map.get_layer_by_name('Ground').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
 
+        for obj in map.get_layer_by_name('Collisions'):
+            CollisionSprite((obj.x, obj.y), pygame.Surface((obj.width, obj.height)), self.collision_sprites)
+
+        for obj in map.get_layer_by_name('Objects'):
+            CollisionSprite((obj.x, obj.y), obj.image, (self.all_sprites, self.collision_sprites))
 
 
 if __name__ == '__main__':
